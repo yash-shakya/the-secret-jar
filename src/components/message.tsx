0e3e303529b54dc8dfbf6f453f0cbe1fd5606419
Yash Shakya <yashshakya40@gmail.com>
2025-07-06 03:03:33 +0530

    another ui

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -19,8 +19,8 @@ function MessageTemplate({message}: {message: string}) {
                 
                 {/* Message content section */}
                 <div className="flex items-center justify-center h-[450px] rounded-b-[100px] bg-white relative overflow-hidden p-4">                    
-                    <div className="relative z-10 text-center max-w-full">
-                        <p className="text-gray-800 text-3xl font-semibold leading-relaxed font-[family-name:var(--font-poppins)] tracking-wide break-words hyphens-auto line-clamp-[11] p-1">
+                    <div className="relative z-10 text-center max-w-full min-h-16">
+                        <p className="text-gray-800 text-3xl my-2 font-semibold leading-relaxed font-[family-name:var(--font-poppins)] tracking-wide break-words hyphens-auto line-clamp-[11] p-1">
                             {message}
                         </p>
                     </div>
